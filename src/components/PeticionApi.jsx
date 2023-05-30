96986ca0be56e0c954db72cc55209269401894d7
Bladimiro Ricaurte <bladimirolcd@gmal.com>
2023-05-30 14:59:50 -0500

    Componente taller review: Variables, PeticionApi y Contador

---
+++ src/components/PeticionApi.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PeticionApi = () => {
+  return (
+    <div>Petición al API de </div>
+  )
+}
+
+export default PeticionApi
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Variables from './components/Variables';
+import Contador from './components/Contador';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Contador />
   </React.StrictMode>
 );
 
